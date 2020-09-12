451cb1fdac590993ecfb2998653eef1820443819
surench <suren.melkonyan@yahoo.com>
2020-09-13 01:17:48 +0400

    Update First Text

---
+++ Assets/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Testing : MonoBehaviour
+{
+	[SerializeField] Text GameTitle;
+
+
+	private void Start()
+	{
+		GameTitle.text = "First Test";
+	}
+}
